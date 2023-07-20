c8b307ece311375a597e5a92c92a3bedbf883633
DLzer <dillon.lomnitzer@gmail.com>
2023-07-20 18:56:14 -0400

    Store message type

--- models/Discord.go
+++ models/Discord.go
@@ -1,18 +1,5 @@
 package models
 
-type DiscordMessageComponentType = int
-
-// DiscordMessageComponent represents a Discord Message model.
-type DiscordMessageComponent struct {
-	Type        DiscordMessageComponentType `json:"type"`
-	Style       int                         `json:"style"`
-	Label       string                      `json:"label"`
-	CustomID    string                      `json:"custom_id"`
-	Url         string                      `json:"url,omitempty"`
-	Disabled    bool                        `json:"disabled"`
-	Placeholder string                      `json:"placeholder"`
-	MinValues   int                         `json:"min_values"`
-	MaxValues   int                         `json:"max_values"`
-	Required    bool                        `json:"required"`
-	Value       string                      `json:"value,omitempty"`
+type DiscordMessage struct {
+	MessageType string
 }
