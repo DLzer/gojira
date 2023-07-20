3b23dde4a5a69c8b4a1f6a67c92b737cb7da915a
DLzer <dillon.lomnitzer@gmail.com>
2023-07-20 18:56:38 -0400

    Store Event ID and Key for dispatch use

--- models/Event.go
+++ models/Event.go
@@ -7,6 +7,8 @@ const (
 
 type EventMap struct {
 	EventType string `json:"eventType"`
+	EventKey  string `json:"eventKey"`
+	EventID   string `json:"eventID"`
 	Created   bool   `json:"created"`
 	Updated   bool   `json:"updated"`
 }
