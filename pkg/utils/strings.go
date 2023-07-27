0066700befd9f55db518e4be5c14b7a559f53f0f
DLzer <dillon.lomnitzer@gmail.com>
2023-07-27 16:23:29 -0400

    Helper fn for searching string slices

---
+++ pkg/utils/strings.go
@@ -0,0 +1,12 @@
+package utils
+
+import "strings"
+
+func StringSliceContains(s []string, e string) bool {
+	for _, a := range s {
+		if strings.ToLower(a) == strings.ToLower(e) {
+			return true
+		}
+	}
+	return false
+}
