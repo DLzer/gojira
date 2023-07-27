91a9daf55754c340a35eeb4a42ef5e840e456844
DLzer <dillon.lomnitzer@gmail.com>
2023-07-27 16:23:46 -0400

    Convert ProjectKey to string slice

--- models/Project.go
+++ models/Project.go
@@ -2,7 +2,7 @@ package models
 
 // ProjectMap represents a structure that holds mappings from JIRA to it's distribution sources
 type ProjectMap struct {
-	ProjectKey           string `json:"ProjectKey,omitempty"`
-	GitHubRepositoryName string `json:"GitHubRepositoryName,omitempty"`
-	DiscordChannelID     string `json:"DiscordChannelID,omitempty"`
+	ProjectKey           []string `json:"ProjectKey,omitempty"`
+	GitHubRepositoryName string   `json:"GitHubRepositoryName,omitempty"`
+	DiscordChannelID     string   `json:"DiscordChannelID,omitempty"`
 }
